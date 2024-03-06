70cf5d83025f17511fd032d4abc9513a2e419d7b
Nicholas Davidson <nicholas.davidson.20@gmail.com>
2024-03-05 20:18:26 -0600

    Created artist model.

--- Models/artist.cs
+++ Models/artist.cs
@@ -2,5 +2,9 @@
 {
     public class Artist
     {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int Age { get; set; }
+        public string? Bio {  get; set; }
     }
 }
